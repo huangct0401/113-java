210d7feb450f7498dc4661079afd01feb2fe3c78
huangct0401 <a112223022@mail.shu.edu.tw>
2024-10-04 11:58:26 +0800

    113-10-04

---
+++ 113-10-04/DecimalDemo2.java
@@ -0,0 +1,17 @@
+import java.math.BigDecimal;
+
+public class DecimalDemo2 {
+    public static void main(String[] args) {
+        var op1 = new BigDecimal("0.1");
+        var op2 = new BigDecimal("0.1");
+        var op3 = new BigDecimal("0.1");
+        var result = new BigDecimal("0.3");
+        
+        if(op1.add(op2).add(op3).equals(result)) {
+            System.out.println("���� 0.3");
+        }
+        else {
+            System.out.println("������ 0.3");
+        }
+    }
+} 
