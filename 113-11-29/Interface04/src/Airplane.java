186be60e29a844beba21bbfb6903bd9f73eae92a
huangct0401 <a112223022@mail.shu.edu.tw>
2024-11-29 17:02:59 +0800

    113-11-29

---
+++ 113-11-29/Interface04/src/Airplane.java
@@ -0,0 +1,14 @@
+public abstract class Airplane implements Flyer {
+    protected String name;
+    
+    public Airplane(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    @Override
+    public void fly() {
+        System.out.printf(" %s 在飛%n", name);
+    }
+}
